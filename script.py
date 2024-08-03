20657642f825ec5c005da95dab1d902b478ee712
Alexander Baranov <alexander05baranov@yandex.ru>
2024-08-03 22:46:07 +0300

    generator -> map in output function

---
+++ script.py
@@ -0,0 +1,17 @@
+
+
+def input_numbers():
+	numbers = []
+
+	while s := input():
+		numbers.append(int(s))
+
+	return numbers
+
+
+def output_numbers(numbers):
+	print(', '.join(map(str, numbers)))
+
+data = input_numbers()
+data = sorted(data)
+output_numbers(data)
